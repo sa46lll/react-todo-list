e1dbc92c5b21d147eb708c0f734fe43f2632dd2b
sa46lll <azure394@gmail.com>
2023-09-17 14:56:22 +0900

    refactor: User 컴포넌트를 Context로 개선

--- api-integrate/src/User.js
+++ api-integrate/src/User.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import axios from 'axios';
-import { useAsync } from 'react-use';
-
-async function getUser(id) {
-  const url = `https://jsonplaceholder.typicode.com/users/${id}`;
-  const response = await axios.get(url);
-  return response.data;
-}
+import React, { useEffect } from 'react';
+import { useUsersState, useUsersDispatch, getUser } from './UsersContext';
 
 function User({ id }) {
-  const state = useAsync(() => getUser(id), [id]);
-  const { loading, error, value: user } = state;
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
+  useEffect(() => {
+    getUser(dispatch, id);
+  }, [dispatch, id]);
+
+  const { data: user, loading, error } = state.user;
 
   if (loading) return <div>로딩중..</div>;
-  if (error) return <div>Error Occured: {error.message}</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
   if (!user) return null;
   return (
     <div>
